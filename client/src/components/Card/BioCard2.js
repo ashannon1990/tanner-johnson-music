22b9fae3e339be85184af34e631fee932ddc8752
ashan <ashannon1990@gmail.com>
2019-12-10 22:49:38 -0500

    cleaned up older code

--- client/src/components/Card/BioCard2.js
+++ client/src/components/Card/BioCard2.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
+// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -61,11 +61,6 @@ export default function BioCard() {
           </Grid>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        {/* <Button size="small" color="primary" href='/contact'>
-          Contact me
-        </Button> */}
-      </CardActions>
     </Card>
   );
 }
